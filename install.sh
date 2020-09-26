27f14803c801900bb8e1ccf85b1bc1145fbc0907
tsung <qaz10130166@gmail.com>
2020-09-26 04:32:11 +0000

    fixed typo

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ apt update
 apt install libgl1-mesa-glx
 pip install --upgrade pip 
 pip install opencv-python
-pip install segmentation-models-pytorch
+#pip install segmentation-models-pytorch
 pip install albumentations
 pip install torch-summary
 pip install ipywidgets
