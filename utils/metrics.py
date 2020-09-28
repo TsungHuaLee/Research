6cda9999a67ec0701bee971b96d2f59fa3d08fd4
tsung <qaz10130166@gmail.com>
2020-09-28 02:05:39 +0000

    remove utils/loss.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -16,26 +16,32 @@ class CrossEntropy(nn.Module):
             self.weight = torch.tensor(self.weight).cuda()
             return nn.CrossEntropyLoss(weight=self.weight)(y_pred, y_gt)
 
-class Fscore(nn.Module):
-    def __init__(self):
+class BiFscore(nn.Module):
+    def __init__(self, epsilon=1e-7):
         super().__init__()
-        self.eps = 1e-7
-        self.threshold = torch.tensor([0.5]).to('cuda')
+        self.epsilon = epsilon
     @property
     def __name__(self):
-        return 'fscore'
-    def forward(self, pred, gt):
-        pred = torch.softmax(pred, dim = 1)
-        pred = torch.argmax(pred, dim = 1)
+        return 'fscore'    
+    def forward(self, y_pred, y_true,):
+        assert y_pred.ndim == 2
+        assert y_true.ndim == 1
+        y_true = F.one_hot(y_true, 2).to(torch.float32)
+        y_pred = F.softmax(y_pred, dim=1)
         
-        tp = (gt * pred).sum().to(torch.float32)
-        tn = ((1 - gt) * (1 - pred)).sum().to(torch.float32)
-        fp = ((1 - gt) * pred).sum().to(torch.float32)
-        fn = (gt * (1 - pred)).sum().to(torch.float32)
-        
-        precision = tp / (tp + fp + self.eps)
-        recall = tp / (tp + fn + self.eps)
-        return (2 * precision * recall + self.eps) / (precision + recall + self.eps)
+        tp = (y_true * y_pred).sum(dim=0).to(torch.float32)
+        tn = ((1 - y_true) * (1 - y_pred)).sum(dim=0).to(torch.float32)
+        fp = ((1 - y_true) * y_pred).sum(dim=0).to(torch.float32)
+        fn = (y_true * (1 - y_pred)).sum(dim=0).to(torch.float32)
+
+        precision = tp / (tp + fp + self.epsilon)
+        recall = tp / (tp + fn + self.epsilon)
+
+        f1 = 2* (precision*recall) / (precision + recall + self.epsilon)
+        f1 = f1.clamp(min=self.epsilon, max=1-self.epsilon)
+        return 1 - f1.mean()
+    
+    
     
 class Specificity(nn.Module):
     def __init__(self):
